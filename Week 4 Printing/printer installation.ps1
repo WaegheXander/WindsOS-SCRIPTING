7b3fed0254fbc401928055e980fc49bc4eb14c8d
Xander Waeghe <xander.waeghe@student.howest.be>
2023-03-24 12:14:32 +0100

    fixed errors

--- Week 4 Printing/printer installation.ps1
+++ Week 4 Printing/printer installation.ps1
@@ -9,44 +9,40 @@ function Install-PrinterServices {
     else {
         Write-Host "> Print and Document Services feature is already installed." -ForegroundColor DarkGreen
     }
-    changeSpool()
+    changeSpool
 }
 
-
 function changeSpool {
-    # change the location of the print spool folder
+    # Define registry path and new spool directory
     $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Print\Printers"
     $newSpoolDirectory = "C:\Spool"
     
-    # Test if the new spool directory exists
+    # Check if new spool directory exists, create it if not
     if (!(Test-Path $newSpoolDirectory)) {
-        # Create new spool directory
         Write-Host "> $newSpoolDirectory directory not found" -ForegroundColor Yellow
-        Write-Host "> Creating new spool directory $newSpoolDirectory." -ForegroundColor Yellow
-        New-Item -ItemType Directory -Path $newSpoolDirectory -WhatIf
+        New-Item -ItemType Directory -Path $newSpoolDirectory | Out-Null
         Write-Host "> The new spool directory $newSpoolDirectory has been created." -ForegroundColor Green
-    }
-    else {
+    } else {
         Write-Host "> The new spool directory $newSpoolDirectory exists." -ForegroundColor Green
     }
     
-    # Check if the registry key exists
+    # Check if the registry key exists and set the new spool directory
     if (Test-Path $registryPath) {
-        if ((Get-ItemProperty -Path $registryPath -Name DefaultSpoolDirectory) -ne $newSpoolDirectory) {
-            Set-ItemProperty -Path $registryPath -Name DefaultSpoolDirectory -Value $newSpoolDirectory -WhatIf
+        $currentSpoolDirectory = Get-ItemProperty -Path $registryPath -Name DefaultSpoolDirectory
+        if ($currentSpoolDirectory -ne $newSpoolDirectory) {
+            Set-ItemProperty -Path $registryPath -Name DefaultSpoolDirectory -Value $newSpoolDirectory
             Write-Host "> The DefaultSpoolDirectory key has been changed to $newSpoolDirectory." -ForegroundColor Green
-        }
-        else {
-            Write-Host "> The DefaultSpoolDirectory key is already configuered correctly" -ForegroundColor Green
+        } else {
+            Write-Host "> The DefaultSpoolDirectory key is already configured correctly." -ForegroundColor Green
         }   
-    }
-    else {
+    } else {
         Write-Host "> Error: The registry key $registryPath does not exist." -ForegroundColor Red
         Write-Host "> Warning: Skipping the change of the DefaultSpoolDirectory key." -ForegroundColor Yellow
-        $defueltSpoonDirectory = Get-ItemProperty -Path $registryPath -Name DefaultSpoolDirectory
-        Write-Host "> Warning: DefaultSpoolDirectory: $defueltSpoonDirectory" -ForegroundColor Yellow
+        if ($currentSpoolDirectory) {
+            Write-Host "> Warning: DefaultSpoolDirectory: $currentSpoolDirectory" -ForegroundColor Yellow
+        }
     } 
-    checkPrinterDriver() 
+    checkPrinterDriver 
 }
 
 function checkPrinterDriver {
@@ -55,16 +51,16 @@ function checkPrinterDriver {
     Write-Host ""
     $ans = Read-Host -Prompt "Do you see your printer driver? (Y/N)"
     while ($ans.ToLower() -ne "") {
-        if ($ans -eq "y") {
+        if ($ans.ToLower() -eq "y") {
             Write-Host "> The printer driver is installed." -ForegroundColor Green
             Write-Host "> Skipping to installing the printer." -ForegroundColor Green
-            installPrinter()
+            installPrinter
         }
-        elseif ($ans -eq "n") {
+        elseif ($ans.ToLower() -eq "n") {
             Write-Host "> The printer driver is not installed." -ForegroundColor Yellow
             Write-Host "> Installing the printer driver." -ForegroundColor Yellow
             # Install the printer driver
-            installPrinterDriver()
+            installPrinterDriver
             Write-Host "> The printer driver has been installed." -ForegroundColor Green
             
         }
@@ -80,40 +76,68 @@ function checkPrinterDriver {
 function installPrinterDriver {
     $url = "https://ftp.hp.com/pub/softlib/software13/COL40842/ds-99374-24/upd-pcl6-x64-7.0.1.24923.exe"
     $outputPath = "C:/Temp"
-    #chek if the path exists else make it
     $outputFile = "driver.exe"
-    #check if the file exists else make it
+
+    # Check if the output directory exists
+    if (!(Test-Path $outputPath)) {
+        Write-Host "> $outputPath directory not found" -ForegroundColor Yellow
+        Write-Host "> Creating new directory $outputPath." -ForegroundColor Yellow
+        New-Item -ItemType Directory -Path $outputPath
+        Write-Host "> The directory $outputPath has been created." -ForegroundColor Green
+    } else {
+        Write-Host "> The directory $outputPath exists." -ForegroundColor Green
+    }
     
+    # Check if the output file exists
+    if (!(Test-Path "$outputPath\$outputFile")) {
+        Write-Host "> $outputPath\$outputFile Ffile not found." -ForegroundColor Yellow
+        Write-Host "> Creating new file $outputFile." -ForegroundColor Yellow
+        New-Item -ItemType File -Path "$outputPath\$outputFile"
+        Write-Host "> The file $outputPath\$outputFile has been created." -ForegroundColor Green
+    } else {
+        Write-Host "> The file $outputPath\$outputFile exists." -ForegroundColor Green
+    }
+
     Write-Host "> Downloading the file from $url to $outputPath\$outputFile" -ForegroundColor Green
     Write-Host "> !!! Don't forget to copy your driver to the path you unzip the file to !!!" -ForegroundColor Yellow
     try {
         # Download the file
         Invoke-WebRequest -Uri $url -OutFile "$outputPath\$outputFile"
-        #run the file
+        
+        # Extract the driver to the output path
+        Write-Host "> Extracting the driver to $outputPath." -ForegroundColor Green
         Start-Process "$outputPath\$outputFile" -ArgumentList "/extract:$outputPath" -Wait
-        #remove the file
-        Remove-Item "$outputPath\$outputFile"
+       
+        # Remove the downloaded file
+        Write-Host "> Removing the downloaded file." -ForegroundColor Green
+        Remove-Item "$outputPath\$outputFile" -Force
         
         Write-Host "> The file has been downloaded to $outputPath\$outputFile" -ForegroundColor Green
         
-        $driverPath = Read-Host -Prompt "The Path U unzip the file to"
+        $driverPath = Read-Host -Prompt "Enter the path where you extracted the driver files:"
         while ($driverPath -eq "" -or (!(Test-Path $driverPath))) {
             Write-Host "> Error: The path does not exist." -ForegroundColor Red
-            $driverPath = Read-Host -Prompt "The Path U unzip the file to"
+            $driverPath = Read-Host -Prompt "Enter the path where you extracted the driver files:"
         }
-        installPrinter()
+
+        $printerDriver = (Get-ChildItem -Path $driverPath -Filter "*.inf").Name
+        Write-Host "> The driver name is $printerDriver" -ForegroundColor Green
+
+        installPrinter
     }
     catch {
         Write-Host "> Error: The file could not be downloaded." -ForegroundColor Red
         $ans = Read-Host -Prompt "Try again? (Y/N)"
         while ($ans.ToLower() -ne "") {
-            if ($ans -eq "y") {
-                installPrinterDriver()
+            if ($ans.ToLower() -eq "y") {
+                installPrinterDriver
                 break
             }
-            elseif ($ans -eq "n") {
-                Write-Host "> Cannot continue without a drive breaking opperation." -ForegroundColor Red
-                #exit the script
+            elseif ($ans.ToLower() -eq "n") {
+                Write-Host "> Cannot continue without a driver. Breaking operation." -ForegroundColor Red
+                pause
+                Get-Process -Name powershell | Stop-Process -Force
+
             }
             else {
                 Write-Host "> Error: Please enter Y or N." -ForegroundColor Red
@@ -126,33 +150,78 @@ function installPrinterDriver {
 function installPrinter {
     # Install and share a network printer, with IP address 172.23.80.3, with that driver.
     $printerName = "HP Printer"
-    $printerDriver = "HP Universal Printing PCL 6"
-    $printerDriverPath = $driverPath
     $printerPort = "IP_172.23.80.3"
     $printerIPAddress = "172.23.80.3"
-    $shareName = "HP_Printer_Shared"
-    $printerLocation = "KWE-A-2-105"
 
     try {
         # Add the printer port
-        Add-PrinterPort -Name $printerPort -PrinterHostAddress $printerIPAddress -WhatIf
+        Add-PrinterPort -Name $printerPort -PrinterHostAddress $printerIPAddress
+
         # Install the printer driver
-        Add-PrinterDriver -Name $printerDriver -InfPath "$printerDriverPath\hpcu187v.inf" -WhatIf
+        Add-PrinterDriver -Name $printerDriver -InfPath "$driverPath\hpcu187v.inf"
+
         # Add the printer
-        Add-Printer -Name $printerName -DriverName $printerDriver -PortName $printerPort -WhatIf
+        Add-Printer -Name $printerName -DriverName $printerDriver -PortName $printerPort
     
         # Share the printer
-        Set-Printer -Name $printerName -Location $printerLocation -Shared $True -ShareName $shareName -WhatIf
+        $ans = Read-Host -Prompt "Do you want to share the printer? (Y/N)"
+        while ($ans.ToLower() -ne "") {
+            if ($ans -eq "y") {
+                $shareName = Read-Host -Prompt "Enter the share name:"
+                while ($shareName -eq "") {
+                    Write-Host "> Error: The share name cannot be empty." -ForegroundColor Red
+                    $shareName = Read-Host -Prompt "Enter the share name:"
+                }
+                $shareLocation = Read-Host -Prompt "Enter the location of the printer:"
+                while ($shareLocation -eq "") {
+                    Write-Host "> Error: The location of the printer cannot be empty." -ForegroundColor Red
+                    $shareLocation = Read-Host -Prompt "Enter the location of the printer:"
+                }
+                Set-Printer -Name $printerName -Location $shareLocation -Shared $True -ShareName $shareName
+                break
+            }
+            elseif ($ans -eq "n") {
+                Write-Host "> The printer is not shared." -ForegroundColor Yellow
+                break
+            }
+            else {
+                Write-Host "> Error: Please enter Y or N." -ForegroundColor Red
+                $ans = Read-Host -Prompt "Do you want to share the printer? (Y/N)"
+            }
+        }
+        
+        Write-Host "> The printer has been installed." -ForegroundColor Green
+        Write-Host "> The printer is shared as $shareName." -ForegroundColor Green
+        Write-Host "> The printer is located at $printerLocation." -ForegroundColor Green
+        Write-Host "> The printer is available at \\$env:COMPUTERNAME\$shareName." -ForegroundColor Green
+        $ans = Read-Host -Prompt "Would you like to print a test page? (Y/N)"
+        while ($ans.ToLower() -ne "") {
+            if ($ans.ToLower() -eq "y") {
+                Write-Host "> Printing a test page." -ForegroundColor Green
+                Print-TestPage
+                break
+            }
+            elseif ($ans.ToLower() -eq "n") {
+                Write-Host "> No test page is printed." -ForegroundColor Green
+                
+                break
+            }
+            else {
+                Write-Host "> Error: Please enter Y or N." -ForegroundColor Red
+                $ans = Read-Host -Prompt "Would you like to print a test page? (Y/N)"
+            }
+        }
+        
     }
     catch {
         Write-Host "> Error: The printer could not be installed." -ForegroundColor Red
         $ans = Read-Host -Prompt "Try again? (Y/N)"
         while ($ans.ToLower() -ne "") {
-            if ($ans -eq "y") {
-                installPrinter()
+            if ($ans.ToLower() -eq "y") {
+                installPrinter
                 break
             }
-            elseif ($ans -eq "n") {
+            elseif ($ans.ToLower() -eq "n") {
                 Write-Host "> No printer is installed" -ForegroundColor Red
                 break
             }
@@ -164,5 +233,20 @@ function installPrinter {
     }
 }
 
+function Print-TestPage {
+    # Print a test page
+    $printer = Get-Printer -Name $printerName
+    if ($printer) {
+        Write-Host "> The printer is installed." -ForegroundColor Green
+        Write-Host "> Printing a test page." -ForegroundColor Green
+        $printer | Out-Printer -FilePath "C:\Windows\System32\spool\drivers\color\A0000001.PPD"
+        Write-Host "> The test page has been queued." -ForegroundColor Green
+    }
+    else {
+        Write-Host "> The printer is not installed." -ForegroundColor Red
+        Write-Host "> Cannot print a test page." -ForegroundColor Red
+    }
+}
+
 $ans, $url, $outputPath, $outputFile, $registryPath, $newSpoolDirectory, $driverPath, $printerName, $printerDriver, $printerDriverPath, $printerPort, $printerIPAddress, $shareName, $printerLocation
-installPrinterServices()
+Install-PrinterServices
