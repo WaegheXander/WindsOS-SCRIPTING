4a39d586d22150648ca0b1dc50fe79190a6c1d6a
XWippie <xander.waeghe@gmail.com>
2023-03-27 21:31:48 +0200

    added dns + promote

--- Week2/ProtmoteToDC.ps1
+++ Week2/ProtmoteToDC.ps1
@@ -1,21 +1,33 @@
-$forestMain = "intranet"
-
 #
 # Check if the script is running as administrator
-#
+#region 
 Write-Host "> Checking permissions"
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Host "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
+    Write-Host "> Error: Insufficient permissions to run this script. Open the PowerShell console as administrator and try again." -ForegroundColor Red
     Pause
     Exit
 }
 else {
-    Write-Host "Code is running as administrator" -ForegroundColor Green
+    Write-Host "> Code is running as administrator" -ForegroundColor Green
 }
+#endregion
+
+#
+# get the nic
+#region
+try {
+    $nic = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias (Get-NetAdapter).Name | Select-Object -ExpandProperty InterfaceIndex)
+}
+catch {
+    Write-Host "> Error: No network adapter found" -ForegroundColor Red
+    Pause
+    Exit
+}
+#endregion
 
 #
 # Check if AD-Domain-Services is installed
-#
+#region
 $feature = Get-WindowsFeature -Name AD-Domain-Services -ErrorAction SilentlyContinue
 if ($feature.Installed -ne "True") {
     # Install AD-Domain-Services feature
@@ -26,125 +38,119 @@ if ($feature.Installed -ne "True") {
 else {
     Write-Host "> AD-Domain-Services feature is already installed." -ForegroundColor Green
 }
+#endregion
 
 #
 # Install Forest if not already installed
-#
+#Region
 Import-Module ADDSDeployment
-# Check if a forest is already installed
-if (Get-ADForest -ErrorAction SilentlyContinue) {
+# Check if a domain controller is already installed
+if (Get-ADDomainController) {
     # A forest is already installed
     Write-Host "> Warining: A forest is already installed." -ForegroundColor Yellow
-    Write-Host "> Getting the forest name" -ForegroundColor Yellow
     #get the forest name
-    $forestName = (Get-ADForest).name
+    $forestName = (Get-WmiObject Win32_ComputerSystem).Domain
+    Write-Host "> Forest name: $forestName" -ForegroundColor Green
+
+    # Check if it is an Primary or a Backup Domain Controller
+    if ((Get-WmiObject -Class Win32_ComputerSystem).DomainRole -eq 5) {
+        # Primary Domain Controller
+        Write-Host "> Primary Domain Controller" -ForegroundColor Green
+    }
+    elseif ((Get-WmiObject -Class Win32_ComputerSystem).DomainRole -eq 4) {
+        # Backup Domain Controller
+        Write-Host "> Backup Domain Controller" -ForegroundColor Green
+    }
+
 }
 else {
-    # No forest is installed
-    Write-Host "> Warning: No forest is installed." -ForegroundColor Yellow
-    $forestTemp = Read-Host "Enter the name of the new forest (intranet.???)"
-    while ($forestTemp -eq "") {
-        Write-Host "The forest name cannot be empty" -ForegroundColor Red
-        $forestTemp = Read-Host "Enter the name of the new forest (INTRANET.???)"
+    $ans = Read-Host "Do you want to create a Primary or a Backup Domain Controller? (P/B)"
+    while ($ans.ToLower() -ne "p" -or $ans.ToLower() -ne "b") {
+        Write-Host "> Error: Invalid answer. Please enter P or B" -ForegroundColor Red
+        $ans = Read-Host "Do you want to create a Primary or a Backup Domain Controller? (P/B)"
     }
-    $forestName = "$forestMain.$forestTemp"
-    Write-Host "> Creating new forest $forestName..." -ForegroundColor Yellow
-    Install-ADDSForest `
-        -DomainName "$forestName" `
-        -DomainNetbiosName "$forestMain" `
-        -ForestMode "Windows2016Forest" `
-        -DomainMode "Windows2016Forest" `
-        -SafeModeAdministratorPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force) `
-        -InstallDns `
-        -NoRebootOnCompletion: $false `
-        -Force:$true
+    if ($ans.ToLower() -eq "p") {
+        install-PrimaryDC
+    }
+    elseif ($ans.ToLower() -eq "b") {
+        install-BackupDC
+    }
 }
 
-#
-# Install Domain if not already installed
-#
-# Check if a domain is already installed
-if (Get-WindowsFeature AD-Domain-Services -ErrorAction SilentlyContinue) {
-    Write-Host "> Warning: This server is already a domain controller." -ForegroundColor Yellow
-} else {
-    # No domain is installed
-    Write-Host "> Warning: No domain is installed." -ForegroundColor Yellow
+# install a Primary Domain Controller 
+function install-PrimaryDC {
+    $NetBiosName = Read-Host "Enter the name of the NetBiosName (ex. INTRANET)"
+    while ($NetBiosName -eq "") {
+        Write-Host "The NetBiosName cannot be empty" -ForegroundColor Red
+        $NetBiosName = Read-Host "Enter the name of the NetBiosName (ex. INTRANET)"
+    }
+    $NetBiosName = $NetBiosName.ToUpper()
+
+    $DomainName = Read-Host "Enter the name of the new forest ($NetBiosName.???)"
+    while ($DomainName -eq "") {
+        Write-Host "The forest name cannot be empty" -ForegroundColor Red
+        $DomainName = Read-Host "Enter the name of the new forest ($NetBiosName.???)"
+    }
+    $forestName = $NetBiosName.$DomainName
+    Write-Host "> Creating new forest $forestName..." -ForegroundColor Yellow
     try {
-        Write-Host "> Getting the network configuration"
-        $adapter = Get-NetAdapter | Where-Object { $_.Status -eq "Up" }
-        if ($adapter) {
-            $ipConfig = Get-NetIPAddress -InterfaceIndex $adapter.ifIndex
-            $defaultGateway = Get-NetRoute -InterfaceIndex $adapter.ifIndex -DestinationPrefix 0.0.0.0/0 | Select-Object -ExpandProperty NextHop
-            $ipAddress = $ipConfig.IPAddress
-            $subnetMask = $ipConfig.PrefixLength
-            $adapterName = $adapter.Name
-            Write-Host "> Network adapter: $($adapterName)"
-            Write-Host "> IP address: $($ipAddress)"
-            Write-Host "> Subnet mask: $($subnetMask)"
-            Write-Host "> Default gateway: $($defaultGateway)"
-        }
-        else {
-            Write-Host "No network adapter is currently up."
-        }
+        Install-ADDSForest `
+            -DomainName $forestName `
+            -DomainNetbiosName $NetBiosName `
+            -DomainMode "WinThreshold" `
+            -ForestMode "WinThreshold" `
+            -InstallDns:$True `
+            -SafeModeAdministratorPassword (ConvertTo-SecureString (Read-Host "Password for PrimaryDC" -AsSecureString) -AsPlainText -Force) `
+            -Force:$True;
+        Write-Host "> Forest $forestName created successfully." -ForegroundColor Green
     }
     catch {
-        Write-Host "> Error: Something went wrong while getting the network configuration." -ForegroundColor Red
-        Write-Error $_.Exception.Message
-        Write-Host "> Exiting..." -ForegroundColor Red
-        Pause
-        exit
-    }
-
-    Write-Host "> This server is not a domain controller."
-    # Create first DC in new forest/domain
-    try {
-        $domainName = Read-Host "Enter the name of the new domain (ex: intranet.local.be)"
-        while ($domainName -eq "") {
-            Write-Host "> Error: The domain name cannot be empty" -ForegroundColor Red
-            $domainName = Read-Host "Enter the name of the new domain (ex: intranet.local.be)"
-        }
-        $siteName = Read-Host "Enter the name of the new site (ex: Brussels)"
-        while ($siteName -eq "") {
-            Write-Host "> Error: The site name cannot be empty" -ForegroundColor Red
-            $siteName = Read-Host "Enter the name of the new site (ex: Brussels)"
-        }
-        $credentials = Get-Credential
-        Write-Host "> Creating new domainController" -ForegroundColor Green
-        Install-ADDSDomainController `
-            -Credential $credentials `
-            -DomainName $domainName `
-            -InstallDns `
-            -NoGlobalCatalog `
-            -SiteName $siteName `
-            -IPAddress $ipAddress `
-            -Force
-    } catch {
-        Write-Host "> Error: Something went wrong while creating the domain controller." -ForegroundColor Red
+        Write-Host "> Error: Something went wrong while creating the forest." -ForegroundColor Red
         Write-Error $_.Exception.Message
     }
 }
+
+# install a secondary domain controller in an existing forest
+function install-BackupDC {
+    $DomainName = (Get-WmiObject Win32_ComputerSystem).Domain
+    Write-Host "> Creating Backup domain controller" -ForegroundColor Yellow
+    try {
+        Install-ADDSDomainController `
+            -DomainMode "WinThreshold" `
+            -DomainMode "WinThreshold" `
+            -DomainName $DomainName `
+            -DomainNetbiosName $DomainName `
+            -SafeModeAdministratorPassword (ConvertTo-SecureString (Read-Host "Password for BackupDC" -AsSecureString) -AsPlainText -Force) `
+            -Force:$True;
+        Write-Host "> Domain $DomainName created successfully." -ForegroundColor Green
+    }
+    catch {
+        Write-Host "> Error: Something went wrong while creating the domain." -ForegroundColor Red
+        Write-Error $_.Exception.Message
+    }
+}
+#endregion
 
 
 #
 # Configure DNS
-#
-$preferredDNSServer = $dcIPAddress
-$alternateDNSServer = $dc2IPAddress
-
-# Check and set local DNS servers
-$nic = Get-NetAdapter | Where-Object {$_.Status -eq "Up" -and $_.Name -eq "Ethernet"} | Select-Object -First 1
-$dnsServers = Get-DnsClientServerAddress -InterfaceIndex $nic.ifIndex | Select-Object -ExpandProperty ServerAddresses
-if ($dnsServers[0] -ne $preferredDNSServer -or $dnsServers[1] -ne $alternateDNSServer) {
-    $dnsServers = @($preferredDNSServer, $alternateDNSServer)
-    Set-DnsClientServerAddress -InterfaceIndex $nic.ifIndex -ServerAddresses $dnsServers
-    Write-Host "Local DNS servers updated."
-} else {
-    Write-Host "Local DNS servers already set correctly."
+#region
+try {
+    $CurrentDns = Get-DnsClientServerAddress -InterfaceIndex $nic;
+    if ($CurrentDns.ServerAddresses -ne $DnsServers) {
+        Write-Host "> Configuring DNS servers..." -ForegroundColor Yellow
+        $primDNS = Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias (Get-NetAdapter).Name | Where-Object { $_.AddressState -eq "Preferred" } | Select-Object -ExpandProperty IPAddress
+        $secDNS = Read-Host "Enter the secondary DNS server"
+        while (!($secDNS -as [IPAddress])) {
+            Write-Host "> Error: Invalid IP address" -ForegroundColor Red
+            $secDNS = Read-Host "Enter the secondary DNS server"
+        }
+        Set-DnsClientServerAddress -InterfaceIndex $nic -ServerAddresses ($primDNS, $secDNS);
+        Write-Host "> DNS servers configured successfully." -ForegroundColor Green
+    }
 }
-
-
-
-
-
-
-
+catch {
+    Write-Host "> Error: Something went wrong while configuring the DNS servers." -ForegroundColor Red
+    Write-Error $_.Exception.Message
+}
+#endregion
