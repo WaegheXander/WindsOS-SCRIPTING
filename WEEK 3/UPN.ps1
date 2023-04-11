a1a681a9654bed1e01bfad90fa4cbd5dde7e1923
XWippie <xander.waeghe@gmail.com>
2023-04-11 18:00:00 +0200

    test upn

--- WEEK 3/UPN.ps1
+++ WEEK 3/UPN.ps1
@@ -1,1 +1,5 @@
-Get-ADForest | Set-ADForest -UPNSuffixes @{add="abc.com"}
+$ans = Read-Host "Enter the UPN suffix you want to add to the forest"
+$forest = Get-ADForest
+$forest.UPNSuffixes.Add($ans)
+$forest | Set-ADForest
+Get-ADForest
