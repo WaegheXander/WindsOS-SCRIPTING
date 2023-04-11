de207e2c6a7d157ff824b93989ef2adbb4454204
XWippie <xander.waeghe@gmail.com>
2023-04-11 18:23:27 +0200

    test upn

--- WEEK 3/UPN.ps1
+++ WEEK 3/UPN.ps1
@@ -4,18 +4,27 @@
 $ans = Read-Host "Do you want to add a UPN suffix? (y/n)";
 while ($true) {
     if ($ans.ToLower() -eq "y") {
-        if (Get-ADForest | Select-Object -ExpandProperty UPNSuffixes | Where-Object {$_ -eq $ans}) {
-            Write-Host "UPN suffix already exists" -ForegroundColor Yellow
-            break;
-        } else {
-            try {
-                Get-ADForest | Select-Object -ExpandProperty UPNSuffixes | Where-Object {$_ -eq '<UPN suffix>'}
-                Write-Host "> UPN suffix successfully created" -ForegroundColor Green
-                break;
+        $upn = Read-Host "Enter the UPN suffix you want to add";
+        while ($true) {
+            if ($upn -eq "") {
+                Write-Host "> UPN suffix cannot be empty" -ForegroundColor Red
+                $upn = Read-Host "Enter the UPN suffix you want to add";
             }
-            catch {
-                Write-Host "Error creating UPN suffix" -ForegroundColor Red
-                Write-Host "> Try again" -ForegroundColor Red
+            else {
+                if (Get-ADForest | Select-Object -ExpandProperty UPNSuffixes | Where-Object { $_ -eq $ans }) {
+                    Write-Host "UPN suffix already exists" -ForegroundColor Yellow
+                }
+                else {
+                    try {
+                        Get-ADForest | Set-ADForest -UPNSuffixes @{add="$upn"};
+                        Write-Host "> UPN suffix successfully created" -ForegroundColor Green
+                        break;
+                    }
+                    catch {
+                        Write-Host "Error creating UPN suffix" -ForegroundColor Red
+                        Write-Host "> Try again" -ForegroundColor Red
+                    }
+                }
             }
         }
     }
@@ -23,7 +32,7 @@ while ($true) {
         break;
     }
     else {
-        Write-Host "Invalid input - please enter 'y' or 'n'" -ForegroundColor Red
+        Write-Host "Invalid input please enter y or n" -ForegroundColor Red
         $ans = Read-Host "Do you want to add a UPN suffix? (y/n)";
     }
 }
