c7a0a849ad1f25e034976135e81466b40bc47bbb
Xander Waeghe <xander.waeghe@student.howest.be>
2023-04-27 11:06:02 +0200

    create ou

---
+++ WEEK 3/OU.ps1
@@ -0,0 +1,21 @@
+$OUNames = Import-Csv ".\csv\OUs.csv" -Delimiter ";"
+ 
+Foreach ($OU in $OUNames)
+{ 
+	$Name = $OU.Name
+	$DisplayName = $OU.DisplayName
+	$Description = $OU.Description
+	$Path = $OU.Path
+	$Identity="OU="+$Name+","+$Path
+
+	try
+	{
+		Get-ADOrganizationalUnit -Identity $Identity | Out-Null
+		Write-Output "> OU $Name already exists in $Path !" -ForegroundColor Yellow
+	}
+	catch
+	{
+		New-ADOrganizationalUnit -Name $Name -DisplayName $DisplayName  -Description $Description -Path $Path
+        set-ADOrganizationalUnit -Identity $Identity -ProtectedFromAccidentalDeletion:$false
+	}
+}
