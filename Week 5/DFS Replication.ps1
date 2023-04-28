b8b0a7c8e49f907a234ccfa41b63cde82bbc42a4
Xander Waeghe <xander.waeghe@student.howest.be>
2023-04-28 12:13:52 +0200

    DFS fix repli

--- Week 5/DFS Replication.ps1
+++ Week 5/DFS Replication.ps1
@@ -49,3 +49,65 @@ Invoke-Command -Session $remoteSession -Scriptblock {
         }
     }
 } -ArgumentList $domainCredential, $secondDC, $MemberServer
+
+
+$DFSRGroup="demo"
+$DFSRFolder="little_demo"
+
+$DFSRMembers=@("win09-ms","win09-dc2")
+$DFSRContentPaths=@("c:\demofolder","c:\demofolder")
+
+try {
+    New-DfsReplicationGroup -GroupName $DFSRGroup -ErrorAction Stop | Out-Null                                                                                      
+    Write-Host "> Created the DFSR Group $DFSRGroup" -ForegroundColor Green
+} catch {
+    Write-Host "> DFSR Group $DFSRGroup already exists" -ForegroundColor Green
+}
+
+try {
+    New-DfsReplicatedFolder -FolderName $DFSRFolder -GroupName $DFSRGroup -ErrorAction Stop | Out-Null
+    Write-Host "> Created the DFSR Folder $DFSRFolder in the DFSR Group $DFSRGroup" -ForegroundColor Green
+} catch {
+    Write-Host "DFSR Folder $DFSRFolder already exists in DFSR Group $DFSRGroup" -ForegroundColor Green
+}
+
+for ($i=0; $i -lt $DFSRMembers.Length; ++$i)
+{
+    $DFSRMember=$DFSRMembers[$i]
+    try {
+        Add-DfsrMember -ComputerName $DFSRMember -GroupName $DFSRGroup -ErrorAction Stop | Out-Null
+        Write-Host "> Adding the DFSR Member $DFSRMember in the DFSR Group $DFSRGroup" -ForegroundColor Green
+    } catch {
+        Write-Host "> The DFSR Member $DFSRMember is already member of the DFSR Group $DFSRGroup" -ForegroundColor Yellow
+    }
+}
+
+$Source=$DFSRMembers[0]
+$Destination=$DFSRMembers[1]
+
+try {
+    Add-DfsrConnection -SourceComputerName $Source -DestinationComputerName $Destination -GroupName $DFSRGroup -ErrorAction Stop | Out-Null
+    Write-Host "> Adding the DFSR Connection between $Source and $Destination" -ForegroundColor Green
+} catch {
+    Write-Host "> The DFSR Connection between $Source and $Destination already exists" -ForegroundColor Yellow
+}
+
+for ($i=0; $i -lt $DFSRMembers.Length; ++$i)
+{
+    try {
+        $DFSRMember=$DFSRMembers[$i]
+        $DFSRContentPath=$DFSRContentPaths[$i]
+        Set-DfsrMembership -ComputerName $DFSRMember -FolderName $DFSRFolder -GroupName $DFSRGroup -ContentPath $DFSRContentPath -Force -ErrorAction Stop | Out-Null
+        Write-Host "> Adding the DFSR Member $DFSRMember with the local path $DFSRContentPath to the DFSR Folder $DFSRFolder in the DFSR Group $DFSRGroup" -ForegroundColor Green
+    } catch {
+        Write-Host "> The DFSR Member $DFSRMember with the local path $DFSRContentPath already added to the DFSR Folder $DFSRFolder in the DFSR Group $DFSRGroup" -ForegroundColor Yellow
+    }
+}
+sleep 5
+
+try {
+    Sync-DfsReplicationGroup -GroupName $DFSRGroup -SourceComputerName $Source -DestinationComputerName $Destination -DurationInMinutes 15 | Out-Null
+    Write-Host "> Synced the DFSR Group $DFSRGroup" -ForegroundColor Green
+} catch{
+    Write-Host "> The DFSR Group $DFSRGroup is already synced of was to fast" -ForegroundColor Yellow
+}
